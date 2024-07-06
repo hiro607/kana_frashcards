9c6c2cd1343f9deb1bf8850e918ad7500c016deb
hiro607 <fumihiro.kawashima@gmail.com>
2024-07-07 00:18:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from PIL import Image
 
 st.title('Kana Flash cards')
 st.markdown('#### Profitez de votre temps libre pour pratiquer les hiragana et les katakana !')
+
+image = Image.open("kana_renshuu.png")
+st.image(image)
