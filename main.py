6813607ec803ac6f214eb5d75102b9c182900d7b
hiro607 <fumihiro.kawashima@gmail.com>
2024-07-07 00:16:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from PIL import Image
 st.title('Kana Flash cards')
 st.markdown('#### Profitez de votre temps libre pour pratiquer les hiragana et les katakana !')
 
-image = Image.open(r"C:\Users\Fumihiro\hiragana_flashcards\data\kana_renshuu.png")
+image = Image.open(r".data\kana_renshuu.png")
 st.image(image)
