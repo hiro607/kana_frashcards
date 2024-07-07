344f7be43fff7458d1117691cab0ceb1d723c5a9
hiro607 <fumihiro.kawashima@gmail.com>
2024-07-07 22:57:51 +0200

    Update hiragana2_dakuon_handakuon_sound.py

--- pages/hiragana2_dakuon_handakuon_sound.py
+++ pages/hiragana2_dakuon_handakuon_sound.py
@@ -21,6 +21,7 @@ if 'remaining_quiz' not in st.session_state:
     st.session_state.score = 0
     st.session_state.hiragana, st.session_state.romanji = get_random_hiragana()
     st.session_state.show_answer = False
+    st.session_state.incorrect_answers = []
 
 remaining_quiz = st.session_state.remaining_quiz
 score = st.session_state.score
@@ -31,11 +32,16 @@ st.title('Hiragana Flash cards Quiz 2')
 # クイズが終了したかどうかを確認
 if remaining_quiz == 0:
     st.markdown(f'### Votre score est {score} points sur 10')
+    st.markdown('### Les kanas incorrects et leur prononciation :')
+    for hiragana, romanji in st.session_state.incorrect_answers:
+        st.markdown(f'- {hiragana}: {romanji}')
+
     if st.button('Commencez un nouveau quiz'):
         st.session_state.remaining_quiz = 10
         st.session_state.score = 0
         st.session_state.hiragana, st.session_state.romanji = get_random_hiragana()
         st.session_state.show_answer = False
+        st.session_state.incorrect_answers = []
         st.experimental_rerun()  # アプリを再実行して新しいクイズを開始
 
 else:
@@ -45,7 +51,7 @@ else:
     romanji = st.session_state.romanji
 
     st.write(f"""
-        <div style="text-align: center; font-size: 300px; font-family: 'Noto Sans JP';">
+        <div style="text-align: center; font-size: 250px; font-family: 'Noto Sans JP';">
             {hiragana}
         </div>
     """, unsafe_allow_html=True)
@@ -64,8 +70,8 @@ else:
         st.experimental_rerun()
 
     if st.button('Incorrect'):
+        st.session_state.incorrect_answers.append((hiragana, romanji))  # 間違った答えを記録
         st.session_state.remaining_quiz -= 1
-        st.session_state.score += 0
         st.session_state.hiragana, st.session_state.romanji = get_random_hiragana()
         st.session_state.show_answer = False
         st.experimental_rerun()
