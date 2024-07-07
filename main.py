0b5a327f4089a9e5e4c7a4a4e870bf051c37a436
hiro607 <fumihiro.kawashima@gmail.com>
2024-07-07 22:27:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.title('Kana Flash cards')
 st.markdown('#### Profitez de votre temps libre pour pratiquer les hiragana et les katakana !')
+
+st.markdown('##### 1. Choisissez l'exercice que vous souhaitez étudier dans la colonne de gauche. 
+\n 1.Commencez la session et pratiquez (un quiz comprend 10 questions). 
+\n 1.À la fin de la session, vos résultats seront affichés. Notez les hiraganas que vous avez mal écrits.')
