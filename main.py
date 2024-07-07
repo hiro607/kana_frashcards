3b8ca3e18c65d3081fa55d8d99ef1e4266283379
hiro607 <fumihiro.kawashima@gmail.com>
2024-07-07 22:34:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ st.markdown('#### Profitez de votre temps libre pour pratiquer les hiragana et l
 st.markdown("""
 1. **Veuillez choisir l'exercice que vous souhaitez étudier dans la colonne de gauche.**
 2. **Commencez la session et pratiquez (un quiz comprend 10 questions par session).**
-3. **À la fin de la session, vos résultats seront affichés. Notez les hiraganas que vous avez mal écrits.**
+3. **À la fin de la session, vos résultats seront affichés. Notez les hiraganas dont vous n'avez pas trouvé la bonne réponse.**
 """)
+
+st.image('kana_renshuu.png')
