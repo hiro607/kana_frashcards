ebb0e4756d4f2c465cf777235c43c3b7f22d1cb5
hiro607 <fumihiro.kawashima@gmail.com>
2024-07-08 08:36:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ st.title('Kana Flash cards')
 st.markdown('#### Profitez de votre temps libre pour pratiquer les hiragana et les katakana !')
 
 st.markdown("""
-1. **Veuillez choisir l'exercice que vous souhaitez étudier dans la colonne de gauche.**
+1. **Choisissez l'exercice que vous souhaitez étudier dans la colonne de gauche.**
 2. **Commencez la session et pratiquez (un quiz comprend 10 questions par session).**
 3. **À la fin de la session, vos résultats seront affichés. Révisons les kanas dont vous n'avez pas trouvé la bonne réponse.**
 """)
